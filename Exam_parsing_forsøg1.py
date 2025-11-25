8009c10b85acc5f7cc671b8af38da9157603d92e
jonasmdsn <47667041+jonasmdsn@users.noreply.github.com>
2025-11-25 11:23:17 +0100

    Add files via upload

--- Exam_parsing_forsøg1.py
+++ Exam_parsing_forsøg1.py
@@ -15,19 +15,26 @@ files = sorted(os.listdir('sinti-en-roma-namenlijst'))
 for file in files:
     text = open('sinti-en-roma-namenlijst/'+file,'r',encoding='utf-8').read()
     soup = BeautifulSoup(text,features="lxml")
-    # pID, name, date of birth and death, place of birth and death
-    matchday = file[:-6]
+    # pID, name, date of birth and death, place of birth and death og alder
+ #   matchday = file[:-6] 
     names = soup.findAll('header',attrs={'class':'c-warvictim-intro'})
     for name in names:
-        #date
-        birthdate = name.find('p',attrs={'class':'c-warvictim-intro__sub'}).text
-        birthdate = birthdate.strip()
-        birthdate = " ".join(birthdate.split())
-        #birthdate = birthdate.strip("\n").split(" ") #få den  til at stoppe ved - og splitte fødselssted ved første , komma
-        #birthdate = birthdate.strip('')
-        print(birthdate)
-
-
+        #makes a list
+        person = []
+        person_info = name.find('p',attrs={'class':'c-warvictim-intro__sub'}).text
+        person_info = person_info.strip()
+        person_info = " ".join(person_info.split())
+#få den  til at stoppe ved - og splitte fødselssted ved første , komma
+        print(person_info)
+        
+        #looks at html files and takes their person ID
+        pID = file.split("-")[0]
+        person.append(pID)
+        #looks at our html files and takes their names
+        pName = file.replace(pID+'-',"")
+        pName = pName.replace(".html","")
+        person.append(pName)
+        
 
    
 
